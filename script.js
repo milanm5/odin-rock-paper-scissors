3e2ccff960b009025c8917eda167001c0a8f816c
milanm5 <milanmiljkovic10012@gmail.com>
2025-03-04 10:55:28 +0100

    Added playRound function and fixed getComputerChoice function

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-console.log("Hello world!");
-console.log(getComputerChoise());
-console.log(getHumanChoice());
+let humanScore, computerScore = 0;
+const computerChoice = getComputerChoise();
+console.log(computerChoice);
+const humanChoice = getHumanChoice();
+
+playRound(humanChoice, computerChoice);
 
 
 function getComputerChoise() {
     let randomChoise = Math.floor((Math.random() * 3)+ 1); 
     switch(randomChoise) {
         case 1: 
-            return "Rock";
+            return "rock";
             break;
         case 2: 
-            return "Paper";
+            return "paper";
             break;
         case 3:
-            return "Scissors";
+            return "scissors";
             break;
     }
 }
@@ -27,4 +30,38 @@ function getHumanChoice() {
     } 
 
     return sign;
+}
+
+function playRound(humanChoice, computerChoice) {
+
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie! You both got " + humanChoice);
+        return;
+    }
+
+    if (humanChoice === "rock") {
+        if (computerChoice === "paper") {
+            console.log("You lose! Paper beats Rock");
+            computerChoice++;
+        } else {
+            console.log("You win! Rock beats Scissors");
+            humanChoice++;
+        }
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            console.log("You win! Paper beats Rock");
+            humanChoice++;
+        } else {
+            console.log("You lose! Scissors beats Paper");
+            computerChoice++;
+        }
+    } else {
+        if (computerChoice === "rock") {
+            console.log("You lose! Rock beats Scissors");
+            computerChoice++;
+        } else {
+            console.log("You win! Scissors beats Paper");
+            humanChoice++;
+        }
+    }
 }
